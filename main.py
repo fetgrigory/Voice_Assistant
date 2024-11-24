222beb9343e5d8d4a7da09664d6cb63232957cfb
fetgrigory <157891679+fetgrigory@users.noreply.github.com>
2024-11-24 21:39:07 +0300

    add

--- main.py
+++ main.py
@@ -1,72 +1,68 @@
-'''
-This program makes a simple voice assistant.
-Author: Fetkulin Grigory, Fetkulin.G.R@yandex.ru
-Starting 23/11/2024
-Ending //
-'''
-import speech_recognition as sr
-import pyttsx3
-sr.pause_threshold = 5
+import os
+import random
+import speech_recognition
+import webbrowser
+import datetime
+
+sr = speech_recognition.Recognizer()
+sr.pause_threshold = 0.5
 
 commands_dict = {
     'commands': {
         'greeting': ['привет', 'приветствую'],
-        'create_task': ['добавить задачу', 'создать задачу', 'заметка']
+        'create_task': ['добавить задачу', 'создать задачу', 'заметка'],
+        'open_website': ['открой браузер'],
+        'about': ['кто ты'],
+        'time': ['сколько время']
+
     }
 }
 
+
 def listen_command():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Говорите...")
-        audio = r.record(source, duration=5)  # Capture audio for 5 seconds
-        try:
-            recognized_text = r.recognize_google(audio_data=audio, language='ru-RU').lower()
-            print(f"You said: {recognized_text}")
-            return recognized_text
-        except sr.UnknownValueError:
-            print("Не поняла")
-            return None
-        except sr.RequestError as e:
-            print(f"Could not request results from Google Speech Recognition service; {e}")
-            return None
+    try:
+        with speech_recognition.Microphone() as mic:
+            sr.adjust_for_ambient_noise(source=mic, duration=0.5)
+            audio = sr.listen(source=mic)
+            query = sr.recognize_google(audio_data=audio,
+                                        language='ru-RU').lower()
+        return query
+    except speech_recognition.UnknownValueError:
+        return 'Damn... Не понял что ты сказал :/'
 
 
-
-# Initialize the text-to-speech engine
-engine = pyttsx3.init()
-
-
-# Define the function to process user commands
-def greeting(message):
-    message = message.lower()
-    if "привет" in message:
-        say_message("Привет друг!")
-    else:
-        say_message("Не понял")
+def greeting():
+    return 'Привет друг!'
 
 
-def create_task(query): 
+def about():
+    return 'Я Голосовой ассистент создан чтобы служить людям!'
+
+
+def create_task():
     print('Что добавим в список дел?')
+    query = listen_command()
     with open('todo-list.txt', 'a', encoding='utf-8') as file:
         file.write(f'❗️ {query}\n')
     return f'Задача {query} добавлена в todo-list!'
 
 
-# Define the function to speak a message
-def say_message(message):
-    print(f"Saying: {message}")
-    engine.say(message)
-    engine.runAndWait()
+def time():
+    now = datetime.datetime.now()
+    formatted_time = now.strftime("%H:%M")
+    return (f"Сейчас {formatted_time}")
+
+
+def open_website():
+    chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s'
+    webbrowser.get(chrome_path).open("https://ya.ru/")
 
 
 def main():
     query = listen_command()
-    if query is not None:
-        for k, v in commands_dict['commands'].items():
-            if query in v:
-                globals()[k](query)
-                break
+    for k, v in commands_dict['commands'].items():
+        if query in v:
+            print(globals()[k]())
 
 
 if __name__ == '__main__':
