ad3272263daae23979a9e67d927bf167449dd53d
fetgrigory <157891679+fetgrigory@users.noreply.github.com>
2024-11-26 17:36:52 +0300

    add open_telegram

--- main.py
+++ main.py
@@ -14,13 +14,14 @@ import random
 
 sr = speech_recognition.Recognizer()
 sr.pause_threshold = 0.5
-
+# settings
 commands_dict = {
     'commands': {
         'greeting': ['привет', 'приветствую'],
         'create_note': ['добавить задачу', 'создать задачу', 'заметка'],
         'music_player': ['музыку'],
         'open_website': ['открой браузер', 'запусти браузер', 'открой google chrome', 'google chrome'],
+        'open_telegram': ['открой telegram'],
         'search_engine': ['поиск'],
         'about': ['кто ты'],
         'time': ['сколько время', 'время', 'текущее время', 'сейчас времени', 'который час']
@@ -95,6 +96,11 @@ def search_engine():
 def open_website():
     chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s'
     wb.get(chrome_path).open("https://ya.ru/")
+    return speak('Открываю')
+
+
+def open_telegram():
+    os.system('C:/Users/Admin/AppData/Roaming/"Telegram Desktop"/Telegram.exe')
     return speak('Открываю')
 
 
