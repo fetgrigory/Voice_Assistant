f1d9a5492e116cde3a498b5bd3a05e479f575feb
fetgrigory <157891679+fetgrigory@users.noreply.github.com>
2024-11-26 20:44:01 +0300

    Modification search_engine

--- main.py
+++ main.py
@@ -5,31 +5,29 @@ Starting 23/11/2024
 Ending //
 '''
 # Installing the necessary libraries
-import speech_recognition
+import speech_recognition as sr
 import webbrowser as wb
 import datetime
 import pyttsx3
 import os
 import random
 from commands import commands_dict
-sr = speech_recognition.Recognizer()
-sr.pause_threshold = 0.5
+r = sr.Recognizer()
+r.pause_threshold = 0.5
 
 
 def listen_command():
-    # Define the function to listen for and recognize user commands
-    try:
-        with speech_recognition.Microphone() as mic:
-            sr.adjust_for_ambient_noise(source=mic, duration=0.5)
-            audio = sr.listen(source=mic)
-            query = sr.recognize_google(audio_data=audio,
-                                        language='ru-RU').lower()
+    with sr.Microphone() as source:
+        print("Слушаю...")
+        audio = r.listen(source)
+        try:
+            query = r.recognize_google(audio_data=audio, language='ru-RU').lower()
             print("Распознано: " + query)
-        return query
-    except speech_recognition.UnknownValueError:
-        speak("Команда не распознана, повторите!")
-    except speech_recognition.RequestError:
-        print("Неизвестная ошибка, проверьте интернет!")
+            return query
+        except sr.UnknownValueError:
+            speak("Команда не распознана, повторите!")
+        except sr.RequestError:
+            print("Неизвестная ошибка, проверьте интернет!")
 
 
 # Initialize the text-to-speech engine
@@ -76,7 +74,7 @@ def search_engine():
     speak('Что искать?')
     query = listen_command()
     wb.open('https://www.google.ru/search?q=' + query)
-    return speak('Сейчас найду')
+    speak('Ищу информацию по запросу ' + query)
 
 
 def open_website():
