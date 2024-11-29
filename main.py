a48261424e8eea6e753d006ecc1b93e021a09453
fetgrigory <157891679+fetgrigory@users.noreply.github.com>
2024-11-29 14:00:19 +0300

    Refactoring

--- main.py
+++ main.py
@@ -11,94 +11,96 @@ import pyttsx3
 import os
 import random
 from commands import commands_dict
-from network import check_seardhing
-
-r = sr.Recognizer()
-r.pause_threshold = 0.5
+from network import NetworkActions
 
 
-def listen_command():
-    with sr.Microphone() as source:
-        print("Слушаю...")
-        audio = r.listen(source)
-        try:
-            query = r.recognize_google(audio_data=audio, language='ru-RU').lower()
-            print("Распознано: " + query)
-            return query
-        except sr.UnknownValueError:
-            speak("Команда не распознана, повторите!")
-        except sr.RequestError:
-            print("Неизвестная ошибка, проверьте интернет!")
+class Assistant:
+    def __init__(self):
+        self.r = sr.Recognizer()
+        self.r.pause_threshold = 0.5
+        self.speak_engine = pyttsx3.init()
+        self.network_actions = NetworkActions()
 
+    def listen_command(self):
+        with sr.Microphone() as source:
+            print("Слушаю...")
+            audio = self.r.listen(source)
+            try:
+                query = self.r.recognize_google(audio_data=audio, language='ru-RU').lower()
+                print("Распознано: " + query)
+                return query
+            except sr.UnknownValueError:
+                self.speak("Команда не распознана, повторите!")
+                return None
+            except sr.RequestError:
+                print("Неизвестная ошибка, проверьте интернет!")
+                return None
 
-# Initialize the text-to-speech engine
-speak_engine = pyttsx3.init()
+    def speak(self, message):
+        print(message)
+        self.speak_engine.say(message)
+        self.speak_engine.runAndWait()
+        self.speak_engine.stop()
 
+    def process_command(self, query):
+        for k, v in commands_dict['commands'].items():
+            if any(command in query for command in v):
+                try:
+                    method = getattr(self, k)
+                    method()
+                except AttributeError:
+                    self.speak(f"Команда '{query}' пока не реализована.")
+                    return
+                return
 
-def greeting(message):
-    message = message.lower()
-    if "привет" in message:
-        speak("Привет друг!")
-    else:
-        speak("Команда не распознана, повторите!")
-
-
-def about():
-    return speak('Я Голосовой ассистент создана чтобы служить людям!')
-
-
-def create_note():
-    print('Что добавим в список дел?')
-    query = listen_command()
-    with open('todo-list.txt', 'a', encoding='utf-8') as file:
-        file.write(f' {query}\n')
-        now = datetime.datetime.now()
-    formatted_datetime = now.strftime("%d.%m.%Y %H:%M:%S")
-    return speak(f'Заметка "{query}" создана от {formatted_datetime}.')
-
-
-def time():
-    # Tell the current time
-    now = datetime.datetime.now()
-    speak("Сейчас " + str(now.hour) + ":" + str(now.minute))
-
-
-def music_player():
-    files = os.listdir('music')
-    # Using os.system to start the sound
-    random_file = f'music/{random.choice(files)}'
-    os.system(f'start {random_file}')
-    return speak(f'Танцуем под {random_file.split("/")[-1]}')
-
-
-def open_telegram():
-    os.system('C:/Users/Admin/AppData/Roaming/"Telegram Desktop"/Telegram.exe')
-    return speak('Открываю')
-
-
-# Define the function to speak a message
-def speak(message):
-    print(message)
-    speak_engine.say(message)
-    speak_engine.runAndWait()
-    speak_engine.stop()
-
-
-def main():
-    # Initial message
-    speak("Гапуся слушает")
-    while True:
-        query = listen_command()
-        # Checking if you need to search the internet
-        wiki_result = check_seardhing(query)
-        # If the result is from Wikipedia
+        wiki_result = self.network_actions.check_searching(query)
         if wiki_result:
-            speak(wiki_result)
+            self.speak(wiki_result)
+
+    def greeting(self):
+        self.speak("Привет друг!")
+
+    def about(self):
+        self.speak('Я Голосовой ассистент, создана чтобы служить людям!')
+
+    def create_note(self):
+        print('Что добавим в список дел?')
+        query = self.listen_command()
+        if query:
+            with open('todo-list.txt', 'a', encoding='utf-8') as file:
+                file.write(f' {query}\n')
+                now = datetime.datetime.now()
+            formatted_datetime = now.strftime("%d.%m.%Y %H:%M:%S")
+            self.speak(f'Заметка "{query}" создана от {formatted_datetime}.')
+
+    def time(self):
+        now = datetime.datetime.now()
+        self.speak("Сейчас " + str(now.hour) + ":" + str(now.minute))
+
+    def music_player(self):
+        files = os.listdir('music')
+        if files:
+            random_file = f'music/{random.choice(files)}'
+            os.system(f'start {random_file}')
+            self.speak(f'Танцуем под {random_file.split("/")[-1]}')
         else:
-            for k, v in commands_dict['commands'].items():
-                if query in v:
-                    print(globals()[k]())
+            self.speak("В папке 'music' нет файлов.")
+
+    def open_telegram(self):
+        try:
+            os.system('C:/Users/Admin/AppData/Roaming/"Telegram Desktop"/Telegram.exe')
+            self.speak('Открываю Telegram')
+        except Exception as e:
+            self.speak(f"Ошибка при открытии Telegram: {e}")
+
+    def main(self):
+        self.speak("Гапуся слушает")
+        while True:
+            query = self.listen_command()
+            if query:
+                self.process_command(query)
 
 
 if __name__ == '__main__':
-    main()
+    assistant = Assistant()
+    assistant.main()
--- network.py
+++ network.py
@@ -4,48 +4,36 @@ Author: Fetkulin Grigory, Fetkulin.G.R@yandex.ru
 Starting 27/11/2024
 Ending //
 '''
-# Installing the necessary libraries
-import webbrowser as wb
 import wikipedia
+import webbrowser as wb
 
+class NetworkActions:
+    def web_search(self, query):
+        if query:
+            wb.open("https://www.google.ru/search?q=" + query)
+            return "Ищу информацию по запросу " + query
+        else:
+            return "Я не поняла, что надо искать."
 
-def web_search(query):
-    # Checking if the query string is empty
-    if query:
-        wb.open("https://www.google.ru/search?q=" + query)
-        return "Ищу информацию по запросу " + query
-    else:
-        # Returning the text for voice-over
-        return "Я не поняла, что надо искать."
+    def wikipedia_search(self, query):
+        try:
+            wikipedia.set_lang("ru")
+            query = query.replace("википедия", "").strip()
+            wiki_result = wikipedia.summary(query, sentences=2)
+            print(f"По данным русской википедии: {wiki_result}")
+            return wiki_result
+        except wikipedia.exceptions.PageError:
+            return "Информация по запросу не найдена в Википедии."
+        except wikipedia.exceptions.DisambiguationError:
+            return "Найдено несколько вариантов, уточните запрос."
+        except Exception as e:
+            return "Я не поняла, что надо искать в Википедии."
 
-
-def wikipedia_search(query):
-    try:
-        # Setting the language to Russian
-        wikipedia.set_lang("ru")
-        # Removing the word "wikipedia" from the query
-        query = query.replace("википедия", "").strip()
-        # We use the Wikipedia API to get information
-        wiki_result = wikipedia.summary(query, sentences=2)
-        print(f"По данным русской википедии: {wiki_result}")
-        # Returning the text for voice-over
-        return wiki_result
-    except wikipedia.exceptions.PageError:
-        return "Информация по запросу не найдена в Википедии."
-    except wikipedia.exceptions.DisambiguationError:
-        return "Найдено несколько вариантов, уточните запрос."
-    except Exception as e:
-        return "Я не поняла, что надо искать в Википедии."
-
-
-def check_seardhing(query):
-    if "найди" in query:
-        query = query.replace("найди", "").strip()
-        return web_search(query)
-    elif "найти" in query:
-        query = query.replace("найти", "").strip()
-        return web_search(query)
-    elif "википедия" in query:
-        return wikipedia_search(query)
-    else:
-        return False
+    def check_searching(self, query):
+        if any(word in query for word in ["найди", "найти"]):
+            query = query.replace("найди", "").replace("найти", "").strip()
+            return self.web_search(query)
+        elif "википедия" in query:
+            return self.wikipedia_search(query)
+        else:
+            return False
